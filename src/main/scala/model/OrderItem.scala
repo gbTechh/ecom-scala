628639a333159531179f058bba8dc9edefd818c1
Santiago Salas Sotillo <112534601+SantiagoSalasucsp@users.noreply.github.com>
2025-06-18 15:38:26 -0500

    Update OrderItem.scala

--- src/main/scala/model/OrderItem.scala
+++ src/main/scala/model/OrderItem.scala
@@ -1,11 +1,24 @@
 package model
 
+import doobie._
+import doobie.implicits._
+import doobie.implicits.javasql._
+import java.sql.Timestamp
+
 case class OrderItem(
     id: Int,
-    orderId: Int, // id_order
-    productId: Int, // id_product
+    orderId: Int,
+    productId: Int,
     quantity: Int,
     unitPrice: Float,
-    createdAt: java.sql.Timestamp,
-    updatedAt: java.sql.Timestamp
+    createdAt: Timestamp,
+    updatedAt: Timestamp
 )
+
+object OrderItem {
+  implicit val orderItemRead: Read[OrderItem] =
+    Read[(Int, Int, Int, Int, Float, Timestamp, Timestamp)].map {
+      case (id, orderId, productId, quantity, unitPrice, createdAt, updatedAt) =>
+        OrderItem(id, orderId, productId, quantity, unitPrice, createdAt, updatedAt)
+    }
+}
